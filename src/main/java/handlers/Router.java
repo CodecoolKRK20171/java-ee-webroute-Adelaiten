c013b3067da286e1d18d611f6e1aedc8e3b9170f
Adelaiten <karoltrzaska19@gmail.com>
2019-01-05 20:09:10 +0100

    Static context class

--- src/main/java/handlers/Router.java
+++ src/main/java/handlers/Router.java
@@ -1,11 +1,17 @@
 package handlers;
 
 import Annotations.WebRoute;
+import com.sun.net.httpserver.HttpExchange;
 
 public class Router {
+    private HttpExchange httpExchange;
 
+    public Router(HttpExchange httpExchange) {
+        this.httpExchange = httpExchange;
+    }
+    @WebRoute(path = "/index")
+    public void onIndex() {
 
-    @WebRoute(path = "/test")
-
+    }
 
 }
